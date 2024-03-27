c0b64bf696e647f39908637bdd1a33a314c41953
yellow <13132565021@163.com>
2024-03-27 23:20:28 +0800

    delete leetcode file

--- leetcode/src/main/java/com/zyh/leetcode/editor/cn/[LCP 37]最小矩形面积.java
+++
@@ -1,8 +0,0 @@
-
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public double minRecSize(int[][] lines) {
-
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
